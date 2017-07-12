9f0023b058c5766d5eab9d5fcabd5aaeecb12a33
MythiliMurugan <mythilimurugan7@gmail.com>
2017-07-12 11:21:58 +0530

    Update alp.c

--- alp.c
+++ alp.c
@@ -3,8 +3,8 @@ void main()
 {
 char a;
 printf("Enter the Character:");
-scanf("%c",a);
-if(((a>='a') && (a<='z'))&&((a>='A') &&(a<='Z')))
+scanf("%c",&a);
+if(((a>='a') && (a<='z'))||((a>='A') &&(a<='Z')))
 printf("Character is alphabet");
 else
 printf("Character is not alphabet");

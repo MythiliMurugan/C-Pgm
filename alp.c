0d68e4642d146a050dfe2b1743e081410d20cd0f
MythiliMurugan <mythilimurugan7@gmail.com>
2017-07-12 11:05:58 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("Enter the Character:");
+scanf("%c",a);
+if(((a>='a') && (a<='z'))&&((a>='A') &&(a<='Z')))
+printf("Character is alphabet");
+else
+printf("Character is not alphabet");
+}

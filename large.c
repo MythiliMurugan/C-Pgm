6746b0e8533c1d3338a59cc26c8bb82f4006fbad
MythiliMurugan <mythilimurugan7@gmail.com>
2017-07-12 11:28:48 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter the three numbers:\n");
+scanf("%d %d %d",&a,&b,&c);
+if((a>c) && (a>c))
+printf("%d is largest number",a);
+else if(b>c)
+printf("%d is largest number",b);
+else
+printf("%d is largest number",c);
+return 0;
+}
